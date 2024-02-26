8dd6cf54036114d79aac1b427ceb6dbbdd922977
Bruno Fernandes <brunofernandes0330@gmail.com>
2024-02-26 19:37:49 -0300

    second commit

---
+++ Poo2024.java
@@ -0,0 +1,12 @@
+package com.mycompany.poo2024;
+
+/**
+ *
+ * @author bruno
+ */
+public class Poo2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
